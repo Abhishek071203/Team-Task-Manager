521062657607b377c40068847f987d28b5ccbf8c
Abhishek071203 <kkumar161972@gmail.com>
2026-05-06 16:59:08 +0530

    fixed railway api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "team-task-manager-production-e3af.up.railway.app",
+  baseURL: "https://team-task-manager-production-e3af.up.railway.app",
 });
 
 export default api;
