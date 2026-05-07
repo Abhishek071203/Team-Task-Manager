1fd2dae3b55e56dbb244f4f054937e50aa36b24f
Abhishek071203 <kkumar161972@gmail.com>
2026-05-07 10:34:49 +0530

    fixed duplicate port declaration

--- backend/index.js
+++ backend/index.js
@@ -77,11 +77,7 @@ app.post("/projects", (req, res) => {
 });
 
 // TEST ROUTE
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
 
 
 // In-memory tasks
